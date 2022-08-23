5a97cc2d374b40752ab0c123e468061c39516a73
rohimoz28 <rohimoz28@gmail.com>
2022-08-23 14:29:58 +0700

    user testing view

--- tests/app/Controllers/AuthTest.php
+++ tests/app/Controllers/AuthTest.php
@@ -1,13 +1,23 @@
 <?php
 
-namespace App\Models;
+namespace CodeIgniter;
 
 use CodeIgniter\Test\CIUnitTestCase;
+use CodeIgniter\Test\ControllerTestTrait;
+use CodeIgniter\Test\DatabaseTestTrait;
 
 class AuthTest extends CIUnitTestCase
 {
-  public function testIndex()
+  use ControllerTestTrait;
+  use DatabaseTestTrait;
+
+  public function testAuthPage()
   {
-    $this->assertTrue(true);
+    $result = $this->withURI('http://localhost:8080/auth')
+      ->controller(\App\Controllers\Auth::class)
+      ->execute('index');
+
+    $this->assertTrue($result->isOK());
   }
 }
+
